a624241871eeb9b10f39bf95cfb17760375078af
Christos Anagnostopoulos <anagnostopoulos@isi.gr>
2022-01-07 12:50:13 +0200

    created new processes

--- src/main/java/gr/isi/iolaos/EmailRecipientsGroupIDs.java
+++
@@ -1,6 +0,0 @@
-package gr.isi.iolaos;
-
-public interface EmailRecipientsGroupIDs {
-	String GR_ESKE_ORGS = "GR_ESKE_ORGS";
-
-}
--- src/main/java/gr/isi/iolaos/ProcessConstants.java
+++
@@ -1,7 +0,0 @@
-package gr.isi.iolaos;
-
-public class ProcessConstants {
-
-  public static final String PROCESS_DEFINITION_KEY = "iolaos"; // BPMN Process ID
-
-}
